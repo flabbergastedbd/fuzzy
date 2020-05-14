5b01c7d24d9fab16c10ea9834af1057e383d2ea7
tunnelshade <tunnelshade@gmail.com>
2020-05-14 16:42:27 +0530

    Made intervals sane for usage

--- src/common/intervals.rs
+++ src/common/intervals.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-const COMMON: u64 = 15;
+const COMMON: u64 = 60;
 
 // Worker related
 pub const WORKER_HEARTBEAT_INTERVAL: Duration = Duration::from_secs(COMMON);
@@ -9,5 +9,5 @@ pub const WORKER_TASK_REFRESH_INTERVAL: Duration = Duration::from_secs(COMMON);
 pub const WORKER_PROCESS_CHECK_INTERVAL: Duration = Duration::from_secs(COMMON);
 
 // Master related
-pub const MASTER_SCHEDULER_INTERVAL: Duration = Duration::from_secs(COMMON);
+pub const MASTER_SCHEDULER_INTERVAL: Duration = Duration::from_secs(60);
 

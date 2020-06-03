78b2fb488060f1c97406dae5920c6b65e113dfd6
tunnelshade <tunnelshade@gmail.com>
2020-06-03 12:45:40 +0530

    Environment variables as second priority for global args

--- src/common/cli.rs
+++ src/common/cli.rs
@@ -10,7 +10,11 @@ use crate::common::xpc::{
     set_ca_crt,
     set_worker_pem
 };
-use crate::common::constants::FUZZY_CONNECT_URL;
+use crate::common::constants::{
+    FUZZY_CONNECT_URL,
+    FUZZY_CA_CERT,
+    FUZZY_CLIENT_PEM,
+};
 
 pub fn parse_volume_map_settings(sub_matches: &ArgMatches) {
     // Set up volume map after verifying
@@ -27,30 +31,31 @@ pub fn parse_volume_map_settings(sub_matches: &ArgMatches) {
     }
 }
 
-fn parse_connect_url(sub_matches: &ArgMatches) -> String {
-    let addr: String;
-    if let Some(connect_addr) = sub_matches.value_of("connect_addr") {
-        addr = connect_addr.to_owned();
+// In order of provided args, environment variable & default
+fn get_arg(sub_matches: &ArgMatches, arg_name: &str, env_key: &str, default: &str) -> String {
+    let value: String;
+    if let Some(argument) = sub_matches.value_of(arg_name) {
+        value = argument.to_owned();
     } else {
-        debug!("Getting connect url from environment variable: {}", FUZZY_CONNECT_URL);
-        addr = match env::var(FUZZY_CONNECT_URL) {
-            Ok(connect_addr) => connect_addr,
-            Err(_) => "https://localhost:12700/".to_owned(),
+        debug!("Getting {} from environment variable: {}", arg_name, env_key);
+        value = match env::var(env_key) {
+            Ok(value) => value,
+            Err(_) => default.to_owned(),
         };
     }
-    addr
+    value
 }
 
 pub fn parse_global_settings(sub_matches: &ArgMatches) {
     // Set up connect addr environment variable
-    let connect_addr = parse_connect_url(sub_matches);
+    let connect_addr = get_arg(sub_matches, "connect_addr", FUZZY_CONNECT_URL, "https://localhost:12700/");
     set_connect_url(&connect_addr);
 
     // Set up connect addr environment variable
-    let ca_cert_path = sub_matches.value_of("ca").unwrap_or("ca.crt");
-    set_ca_crt(ca_cert_path);
+    let ca_cert_path = get_arg(sub_matches, "ca", FUZZY_CA_CERT, "ca.crt");
+    set_ca_crt(&ca_cert_path);
 
     // Set up connect addr environment variable
-    let worker_pem_path = sub_matches.value_of("worker_pem").unwrap_or("worker.pem");
-    set_worker_pem(worker_pem_path);
+    let worker_pem_path = get_arg(sub_matches, "worker_pem", FUZZY_CLIENT_PEM, "worker.pem");
+    set_worker_pem(&worker_pem_path);
 }

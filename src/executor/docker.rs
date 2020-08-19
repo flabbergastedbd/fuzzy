c48ceebd72451a670f3e8ebe50a80892c71dfe1a
tunnelshade <tunnelshade@gmail.com>
2020-08-19 16:57:17 +0530

    Added error logging and fixed docker login issues

--- src/executor/docker.rs
+++ src/executor/docker.rs
@@ -249,6 +249,9 @@ async fn force_pull_image(image: String) -> Result<(), Box<dyn Error>> {
     let output = child.wait_with_output().await?;
 
     if output.status.success() == false {
+        error!("Image pull exited with status: {:?}", output.status.code());
+        error!("Stdout: {:?}", str::from_utf8(&output.stdout).unwrap_or("Couldn't UTF-8 decode stdout"));
+        error!("Stderr: {:?}", str::from_utf8(&output.stderr).unwrap_or("Couldn't UTF-8 decode stderr"));
         Err(
             Box::new(std::io::Error::new(
                 std::io::ErrorKind::InvalidData,
